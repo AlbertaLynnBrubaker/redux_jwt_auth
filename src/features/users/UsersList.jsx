46e7fffb94e63c2f27048aa878626489a6967d2c
Alie Brubaker <findingalberta@gmail.com>
2024-01-10 17:34:45 -0800

    merging to main

---
+++ src/features/users/UsersList.jsx
@@ -0,0 +1,25 @@
+// import { useGetUsersQuery } from "./usersApiSlice";
+// import { Link } from "react-router-dom";
+
+// const UsersList = () => {
+//     const {
+//         data: users,
+//         isLoading,
+//         isSuccess,
+//         isError,
+//         error
+//     } = useGetUsersQuery()
+
+//     let usersList
+//     if(isLoading) {
+//         usersList = <p>"Loading...</p>
+//     } else if (isSuccess) {
+//         usersList
+//     }
+
+//   return (
+//     <div>UsersList</div>
+//   )
+// }
+
+// export default UsersList
---
+++ src/features/users/usersApiSlice.js
@@ -0,0 +1,18 @@
+// import { apiSlice } from "../../app/api/apiSlice";
+
+// export const usersApiSlice = apiSlice.injectEndpoints({
+//     endpoints: builder => ({
+//         getUsers: builder.query({
+//             query: () => ({
+//                 url: '/users',
+//                 keepUnusedDataFor: 5,
+//                 method: 'GET'
+//             })
+//         }),
+
+//     })
+// })
+
+// export const {
+//     useGetUsersQuery
+// } = usersApiSlice
