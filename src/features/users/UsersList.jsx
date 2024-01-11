081e30b77c8c76634af85ef30c4448aacc5207a5
Alie Brubaker <findingalberta@gmail.com>
2024-01-10 18:20:10 -0800

    WIP

--- src/features/users/UsersList.jsx
+++ src/features/users/UsersList.jsx
@@ -1,25 +1,35 @@
-// import { useGetUsersQuery } from "./usersApiSlice";
-// import { Link } from "react-router-dom";
+import { useGetUsersQuery } from "./usersApiSlice";
+import { Link } from "react-router-dom";
 
-// const UsersList = () => {
-//     const {
-//         data: users,
-//         isLoading,
-//         isSuccess,
-//         isError,
-//         error
-//     } = useGetUsersQuery()
+const UsersList = () => {
+    const {
+        data: users,
+        isLoading,
+        isSuccess,
+        isError,
+        error
+    } = useGetUsersQuery()
 
-//     let usersList
-//     if(isLoading) {
-//         usersList = <p>"Loading...</p>
-//     } else if (isSuccess) {
-//         usersList
-//     }
+    let usersListContent
+    if (isLoading) {
+        usersListContent = <p>"Loading...</p>
+    } else if (isSuccess) {
+        usersListContent = (
+            <section className="users">
+                <h1>Users List</h1>
+                <ul>
+                    {users.map((user, i) => {
+                        return <li key={i}>{user.username}</li>
+                    })}
+                </ul>
+                <Link to='/welcome'>Back to Welcome</Link>
+            </section>
+        )
+    } else if (isError) {
+        usersListContent = <p>{JSON.stringify(error)}</p>
+    }
 
-//   return (
-//     <div>UsersList</div>
-//   )
-// }
+    return usersListContent
+}
 
-// export default UsersList
+export default UsersList
--- src/features/users/usersApiSlice.js
+++ src/features/users/usersApiSlice.js
@@ -1,18 +1,18 @@
-// import { apiSlice } from "../../app/api/apiSlice";
+import { apiSlice } from "../../app/api/apiSlice";
 
-// export const usersApiSlice = apiSlice.injectEndpoints({
-//     endpoints: builder => ({
-//         getUsers: builder.query({
-//             query: () => ({
-//                 url: '/users',
-//                 keepUnusedDataFor: 5,
-//                 method: 'GET'
-//             })
-//         }),
+export const usersApiSlice = apiSlice.injectEndpoints({
+    endpoints: builder => ({
+        getUsers: builder.query({
+            query: () => ({
+                url: '/users',
+                keepUnusedDataFor: 5,
+                method: 'GET'
+            })
+        }),
 
-//     })
-// })
+    })
+})
 
-// export const {
-//     useGetUsersQuery
-// } = usersApiSlice
+export const {
+    useGetUsersQuery
+} = usersApiSlice
